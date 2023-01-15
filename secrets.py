40872eb159bdf1033409b9b4d8fa875c2579e39b
rsoto15 <111111371+rsoto15@users.noreply.github.com>
2023-01-15 11:45:01 -0700

    Delete secrets.py

--- secrets.py
+++
@@ -1,6 +0,0 @@
-secrets = {
-    'MYSQL_HOST': "iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    'MYSQL_USER': "sciykvdb2o5i0e0l",
-    'MYSQL_PASSWORD': "v065j0eo3fqz33iq",
-    'MYSQL_DB': "c5c3m21wyx69qw37"
-}
